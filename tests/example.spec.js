d9144a5e1ee47d8fec0e4e359412c7f7bbff5d71
Aiswarya Paramadathil <107682605+AishParamadathil@users.noreply.github.com>
2025-04-10 20:04:27 -0400

    completes tests 1,2 and 4

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,7 +1,7 @@
 // @ts-check
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
+/*test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
@@ -16,4 +16,15 @@ test('get started link', async ({ page }) => {
 
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+});*/
+
+
+
+//Login using standard user and perform assertion on username and password
+test('Login using standard user', async ({ page }) => {
+  await page.goto("https://www.saucedemo.com");
+  await page.fill(input[#user-name], 'standard_user');
+  await page.fill('input[#password]', 'secret_sauce');
+  console.log('User logged in to the application');
+  await expect(page.locator('.title')).toContainText('Products'); //there is a class '.title' which contains the text 'Products'
 });
--- tests/test.js
+++
@@ -1,3 +0,0 @@
-// @ts-check
-import { test, expect } from '@playwright/test';
-

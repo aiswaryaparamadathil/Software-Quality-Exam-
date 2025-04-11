48b16d5cd88b7f1351682310b2a0d0f2bb2d13f9
Aiswarya Paramadathil <107682605+AishParamadathil@users.noreply.github.com>
2025-04-10 20:15:44 -0400

    Added remaining tests

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -2,7 +2,7 @@
 import { test, expect } from '@playwright/test';
 
 
-/*Login using standard user and perform assertion on username and password
+Login using standard user and perform assertion on username and password
 test('Login using standard user', async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
   await page.fill('#user-name', 'standard_user');
@@ -37,7 +37,7 @@ test('To add to cart', async ({ page }) => {
   await page.click('.shopping_cart_link');
   await expect(page.locator('.inventory_item_name')).toContainText('Sauce Labs Backpack');
   
-});*/
+});
 
 
 //About
@@ -66,65 +66,6 @@ test('About', async ({ page }) => {
 
 
 
-
-
-
-/*test('Test case 1', async ({browser}) =>
-{
-    const context=await browser.newContext();
-    const page=await context.newPage();
-    await page.goto("https://www.google.com");
-
-}); 
-
-
-// Test case 1: Use Top Navigation to Visit Cart Page
-test('Use top nav to visit cart page', async ({ page }) => {
-    await page.goto('https://www.automationexercise.com');
-    await page.click('a[href="/view_cart"]');
-    console.log('Navigated to the cart page using the top navigation');
-    await expect(page.locator('.active')).toContainText('Shopping Cart'); //there is a class '.active' which contains the text 'Shopping Cart'
-  });
-
-
-  // Test case2: Test Scroll-to-Top Button
-test('Test scroll-to-top button', async ({ page }) => {
-    await page.goto('https://www.automationexercise.com');
-    await page.evaluate(() => window.scrollTo(0, document.body.scrollHeight)); //This is a built-in browser function that scrolls the page to a specific position.
-    await page.click('#scrollUp');
-    console.log('Clicked scroll up button');
-    await expect(page).toHaveURL('https://www.automationexercise.com');
-  });
-  
-  
-  // Test case 3: Homepage Sliders Auto-Rotate
-  test('Homepage sliders auto-rotate', async ({ page }) => {
-    await page.goto('https://www.automationexercise.com');
-    await page.waitForTimeout(6000); // Wait for the slider to rotate
-    console.log('Observed homepage slider auto-rotation');
-    await expect(page.locator('#slider-carousel')).toBeVisible();
-  });
-  
-  //Test case 4: Checkout with Empty Cart
-  test('Checkout with empty cart', async ({ page }) => {
-    await page.goto('https://www.automationexercise.com/view_cart');
-    console.log('Checked out with empty cart');
-    await expect(page.locator('a[class="btn btn-default check_out"]')).toBeHidden();
-  });
-  
-  
-  // Test case 5: Submit Empty Contact Form
-  test('Submit empty contact form', async ({ page }) => {
-    await page.goto('https://www.automationexercise.com/contact_us');
-    await page.click('input[name="submit"]');
-    console.log('Submitted empty contact form');
-    await expect(page.locator('.status.alert-success')).not.toBeVisible();
-  });*/
-
-
-
-  //Login using standard user and perform assertion on username and password.
-  
 
 
 
